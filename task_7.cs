5b3ae736872afe60963e0eef339af295a1fd5215
light_ <78278078+Light-UA@users.noreply.github.com>
2024-04-13 19:24:37 +0300

    Update task_7.cs

--- task_7.cs
+++ task_7.cs
@@ -4,39 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random random = new Random();
+        int maxEmployees = 0;
 
-        int[] password = new int[8];
-
-        do
+        // Цикли для генерації паролей та підрахунку максимальної кількості співробітників
+        for (int d1 = 1; d1 <= 6; d1++)
         {
-            for (int i = 0; i < 8; i++)
+            for (int d2 = 1; d2 <= 6; d2++)
             {
-                // Генеруємо випадкову цифру від 1 до 6
-                password[i] = random.Next(1, 7);
+                for (int d3 = 1; d3 <= 6; d3++)
+                {
+                    for (int d4 = 1; d4 <= 6; d4++)
+                    {
+                        for (int d5 = 1; d5 <= 6; d5++)
+                        {
+                            for (int d6 = 1; d6 <= 6; d6++)
+                            {
+                                for (int d7 = 1; d7 <= 6; d7++)
+                                {
+                                    for (int d8 = 1; d8 <= 6; d8++)
+                                    {
+                                        int sum = d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8;
+                                        if (sum % 8 == 0)
+                                        {
+                                            maxEmployees++;
+                                            Console.WriteLine($"{d1}{d2}{d3}{d4}{d5}{d6}{d7}{d8}");
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
-        } while (!IsPasswordValid(password));
-
-        Console.WriteLine("Згенерований пароль:");
-        foreach (int digit in password)
-        {
-            Console.Write(digit);
         }
 
+        // Виведення максимальної кількості співробітників
+        Console.WriteLine($"Максимальна кількість співробітників: {maxEmployees}");
+
+        // Завершення програми
         Console.ReadKey();
-    }
-
-    static bool IsPasswordValid(int[] password)
-    {
-        // Перевіряємо, чи виконується умова суми трьох сусідніх цифр
-        for (int i = 0; i < 6; i++)
-        {
-            if (password[i] + password[i + 1] + password[i + 2] != 8)
-            {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
